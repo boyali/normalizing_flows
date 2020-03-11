8c590c5e74b939856a6d73cd80fd4c0f03d506af
ali <boyali@gmail.com>
2020-03-11 17:56:04 +0900

    some fixes

--- maf.py
+++ maf.py
@@ -86,10 +86,12 @@ def create_masks(input_size, hidden_size, n_hidden, input_order='sequential', in
 
     # set input degrees to what is provided in args (the flipped order of the previous layer in a stack of mades);
     # else init input degrees based on strategy in input_order (sequential or random)
+
     if input_order == 'sequential':
         degrees += [torch.arange(input_size)] if input_degrees is None else [input_degrees]
         for _ in range(n_hidden + 1):
             degrees += [torch.arange(hidden_size) % (input_size - 1)]
+
         degrees += [torch.arange(input_size) % input_size - 1] if input_degrees is None else [
             input_degrees % input_size - 1]
 
@@ -98,6 +100,7 @@ def create_masks(input_size, hidden_size, n_hidden, input_order='sequential', in
         for _ in range(n_hidden + 1):
             min_prev_degree = min(degrees[-1].min().item(), input_size - 1)
             degrees += [torch.randint(min_prev_degree, input_size, (hidden_size,))]
+
         min_prev_degree = min(degrees[-1].min().item(), input_size - 1)
         degrees += [torch.randint(min_prev_degree, input_size, (input_size,)) - 1] if input_degrees is None else [
             input_degrees - 1]
